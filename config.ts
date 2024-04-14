3c6330a29a6e5148aee50c6ead6735d459b4004f
Maxime Moreillon <m.moreillon@gmail.com>
2024-04-15 07:50:51 +0900

    default org

--- config.ts
+++ config.ts
@@ -2,9 +2,9 @@ export const {
   GRAFANA_ADMIN_USERNAME = "admin",
   GRAFANA_ADMIN_PASSWORD = "password",
   GRAFANA_URL = "http://localhost:3000",
+  GRAFANA_DEFAULT_ORG_ID = "1",
   JWT_SECRET = "sh...",
   TOKEN_COOKIE = "self_grafana_token",
-  GRAFANA_DEFAULT_ORG_ID = "1",
 } = process.env
 
 export const encodedJwtSecret = new TextEncoder().encode(JWT_SECRET)
